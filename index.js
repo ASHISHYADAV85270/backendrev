ceefc2e49ae0289239fa16132b766f4049d98066
Ashish Yadav <savitayadavmansarover@gmail.com>
2023-10-16 22:54:58 +0530

    updated important text

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const myServer = http.createServer(
-    (req, res) => {
-        // console.log(`new request recieved`);
-        // console.log({ req });
-        // console.log(res)
-        // console.log(req.headers);
-        const parsedurl = url.parse(req.url, true);
+const express = require('express');
 
-        const log = `${Date.now()}: New Req at ${parsedurl.pathname} Received \n`;
-        if (req.url === '/favicon.ico') {
-            return res.end();
-        }
-        fs.appendFile('./log.text', log, (err, result) => {
-            console.log(parsedurl);
-            switch (parsedurl.pathname) {
-                case '/':
-                    res.end("home page");
-                    break;
-                case '/about':
-                    const userrollnumber = parsedurl.query.rollnumber;
-                    res.end(` ${userrollnumber} about page`);
-                    break;
-                case '/search':
-                    const data_searched = parsedurl.query.search_query;
-                    res.end(`here is the result for --> ${data_searched}`);
-                    break;
-                default:
-                    res.end("404");
-            }
+const app = express();
 
-            console.log('file appended successfully');
-        });
-    }
-);
+app.get('/', (req, res) => {
+    return res.send("home page")
+})
+app.get('/about', (req, res) => { res.send(`about page ${req.query.name} `) });
 
-myServer.listen(9000, () => {
-    console.log(`server Started`);
-});
+
+app.listen(5000, () => { console.log("server created") })
