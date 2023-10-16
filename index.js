48c5755e7d069698dace1627e361134501f4b4f0
Ashish Yadav <savitayadavmansarover@gmail.com>
2023-10-16 23:16:53 +0530

    done with some basic tasks

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const express = require('express');
+/* Task 
 
+//hybrid server
+GET /users -HTML document Render 
+GET/api/users- List all users JSON
+
+**DYNAMIC PATH PARAMS
+GET /users/:id -Get user by id
+
+
+POST /users -Create new user
+PATCH /users/:id -Update user by id 
+DELETE /users/:id -Delete user by id
+
+//free json mockaroo.com
+
+*/
+
+
+const express = require('express');
 const app = express();
+const PORT = 8000;
+
+const users = require('./MOCK_DATA.json');
 
 app.get('/', (req, res) => {
-    return res.send("home page")
-})
-app.get('/about', (req, res) => { res.send(`about page ${req.query.name} `) });
+    res.send("Hello Ashish");
+});
 
 
-app.listen(5000, () => { console.log("server created") })
+app.get('/users', (req, res) => {
+    const html = `<ul>
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    </ul > `;
+    res.send(html);
+});
+
+//REST API
+app.get('/api/users', (req, res) => {
+    res.json(users);
+});
+
+
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id); // same hota hai name
+    const user = users.find(user => user.id === id);
+    res.json(user);
+});
+
+
+//browser by default sends get request --> so we will use here POSTMAN
+app.post('/api/users', (req, res) => { });
+app.listen(PORT, () => { console.log(`server running on port ${PORT} `) });
